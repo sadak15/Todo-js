beee12c2de40763b6188f8ae8ee24e7b22f23ff2
sadak15 <sadakc15@gmail.com>
2026-03-30 18:56:28 +0300

    last update

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 
+document.addEventListener("DOMContentLoaded", loadTasks);
+
+function loadTasks(){
+
+    const tasks = getTasksFromlocalStorage();
+
+    tasks.forEach(task => {
+        addTaskToDOM(task)
+        
+    });
+
+};
+
 //adding submit
 
 todoForm.addEventListener("submit", addTask);
@@ -23,6 +36,7 @@ function addTask(e) {
 
         }
         addTaskToDOM(task);
+        saveTaskToLocal(task)
 
         todoInput.value = "";
 
@@ -43,8 +57,37 @@ function addTaskToDOM(task) {
 todoList.appendChild(li);
 
 
+attachEventListener(li, task)
+
+
+}
+function attachEventListener(li, task){
+    const deleteBtn = li.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", function(){
+        handleDelete(task.id, li)
+    })
+
 }
 
+function handleDelete(id, li){
+    let tasks = getTasksFromlocalStorage();
+    tasks = tasks.filter(task => task.id != id);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    li.remove();
+    console.log(tasks)
+}
+function saveTaskToLocal(task){
+    const oldTasks = getTasksFromlocalStorage();
 
+    oldTasks.push(task);
 
+    localStorage.setItem("tasks", JSON.stringify(oldTasks))
+}
+
+function getTasksFromlocalStorage(){
+    const oldTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    return oldTasks
+
+}
 
